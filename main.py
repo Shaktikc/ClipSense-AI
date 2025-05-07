dd3eaae9f34a86206ac911587290085a9ed3a7a4
Shaktikc <shakti3462@gmail.com>
2025-05-07 19:28:19 +0545

    added the mistral llm

--- main.py
+++ main.py
@@ -2,6 +2,30 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from youtube_transcript_api import YouTubeTranscriptApi
 from typing import List, Dict
+import os
+from mistralai import Mistral
+from dotenv import load_dotenv
+
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+api_key = os.environ["API_KEY"]
+model = "mistral-large-latest"
+
+client = Mistral(api_key=api_key)
+
+chat_response = client.chat.complete(
+    model= model,
+    messages = [
+        {
+            "role": "user",
+            "content": "What is the best French cheese?",
+        },
+    ]
+)
+print(chat_response.choices[0].message.content)
 
 
 app = FastAPI()
