f7be339255ca22d7fc52597cd45f4a818676bc9a
Shaktikc <shakti3462@gmail.com>
2025-05-27 18:01:56 +0545

    working on video source reference

--- app/api/video_summerizer_routes.py
+++ app/api/video_summerizer_routes.py
@@ -82,7 +82,7 @@ def get_youtube_videos_summary(
             saved_video_paths.append(tmp_path)
 
         # Pass all video paths at once
-        mergedVideo_userQuery_service.py(
+        mergedVideo_userQuery_service(
             saved_video_paths,  # Now passing list of paths
             user_query_match_transcript_objs,
             video_ids,
